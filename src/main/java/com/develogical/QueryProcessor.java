64e9e2f9483c7773c7a657437047bc169baca7bb
David Lohse <david.lohse@tum.de>
2022-07-05 10:32:18 +0200

    eist

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,6 +7,9 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        } else if (query.toLowerCase().contains("eist")) {
+            return "EIST is the most beloved subject at TUM, with students frequently " +
+                    "failing the exam intentionally just so they can enjoy the class again.";
         }
         return "";
     }
